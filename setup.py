2cffe7d2dc04e1051894357e8bbe5d1568983cc8
PlayWitIt <PlayWitItMyWay@Gmail.com>
2026-04-19 07:52:03 -0400

    Fix author to PlayWit Creations

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="BuM-BackupManager",
     version="0.1.0",
     description="A Textual-based backup manager with scheduling and archive management",
-    author="PlayWit",
+    author="PlayWit Creations",
     author_email="playwititmyway@gmail.com",
     package_dir={"": "src"},
     packages=find_packages("src"),
